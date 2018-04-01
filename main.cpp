82762cd72cf1b28a151dd2f9ec23613de0a2aaaa
Cem Üyük <cemuyuk@Cem-MacBook-Pro.local>
2018-04-01 18:06:30 +0300

    Key tap added, somewhat works. It still needs to be more efficient. Couldn't implement Screen Tap...

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@
 #include "Leap.h"
 
 using namespace Leap;
-bool exCode=true;
+bool exCode=false;
 
 class SampleListener : public Listener {
 public:
@@ -31,6 +31,7 @@ void SampleListener::onFrame(const Controller& controller) {
     
 
          for(Leap::GestureList::const_iterator gl = gestures.begin();
+             
              gl != frame.gestures().end(); gl++){
          
              if((*gl).type() == Leap::Gesture::TYPE_CIRCLE){
@@ -38,23 +39,63 @@ void SampleListener::onFrame(const Controller& controller) {
                          cGesture = Leap::CircleGesture(*gl);
                          if(cGesture.radius()>90){
                              std::cout << "Circle." << std::endl;
-                             exCode=false;
+                             exCode=true;
                          }
-                 }
-             if((*gl).type()==Leap::Gesture::TYPE_SWIPE){
+             }else if((*gl).type()==Leap::Gesture::TYPE_SWIPE){
                  Leap::SwipeGesture swipeGesture = Leap::Gesture::invalid();
                  swipeGesture = Leap::SwipeGesture(*gl);
                  Leap::Vector swipeDirection = swipeGesture.direction();
                  
-                 if(swipeDirection.x < 0 && swipeGesture.position().x < -30){
+                 if(swipeGesture.pointable().hand().palmNormal().x < 0 || swipeGesture.pointable().hand().palmNormal().x > 0 ){
+                     if(swipeDirection.x < 0 && swipeGesture.position().x < -40){
                      std::cout << "Left direction" << std::endl;
-                 }
-                     if(swipeDirection.x > 0 && swipeGesture.position().x > 30){
+                     }
+                     if(swipeDirection.x > 0 && swipeGesture.position().x > 40){
                          std::cout << "Right direction" << std::endl;
                      }
                  }
-                 if(!exCode)
-                     break;
+                 
+              
+                 
+             }else{
+                 Leap::Pointable ptbl = (*gl).pointables().frontmost();
+                 
+                 if(ptbl.ZONE_TOUCHING){
+                     std::cout << "Key Tap buddy." << std::endl;
+                 }else{
+                     std::cout << "Screen Tap buddy." << std::endl;
+                 }
+             }
+             
+             /*
+             else  if((*gl).type()==Leap::Gesture::TYPE_SCREEN_TAP){
+                
+                 
+                Leap::ScreenTapGesture scrTapGest = Leap::Gesture::invalid();
+                scrTapGest = Leap::ScreenTapGesture(*gl);
+                 
+                Leap::Pointable scrTapPointable = scrTapGest.pointable();
+                
+                if(scrTapPointable.hand().palmNormal().y < 0 &&
+                   scrTapPointable.ZONE_HOVERING){
+                        std::cout << "Screen Tap buddy." << std::endl;
+                }
+                 
+             }else if((*gl).type()==Leap::Gesture::TYPE_KEY_TAP){
+                 
+                Leap::KeyTapGesture keyTapGest = Leap::Gesture::invalid();
+                keyTapGest = Leap::KeyTapGesture(*gl);
+                 
+                Leap::Pointable keyTapPointable = keyTapGest.pointable();
+                
+                 if(keyTapPointable.hand().palmNormal().y < 0
+                    && keyTapPointable.ZONE_TOUCHING){
+                        std::cout << "Key Tap buddy." << std::endl;
+                    
+                }
+             }
+             */
+             if(exCode) break;
          }
 }
 
@@ -64,16 +105,28 @@ int main(int argc, const char * argv[]) {
     SampleListener listener;
     Controller controller;
     
+    /*
+    controller.config().setFloat("Gesture.KeyTap.MinDownVelocity", 30.0);
+    controller.config().setFloat("Gesture.KeyTap.HistorySeconds", .2);
+    controller.config().setFloat("Gesture.KeyTap.MinDistance", 6.0);
+    controller.config().save();
+    
+    controller.config().setFloat("Gesture.ScreenTap.MinDownVelocity", 30.0);
+    controller.config().setFloat("Gesture.ScreenTap.HistorySeconds", .2);
+    controller.config().setFloat("Gesture.ScreenTap.MinDistance", 6.0);
+    controller.config().save();
+     */
+    
     controller.enableGesture(Gesture::TYPE_CIRCLE);
-    controller.enableGesture(Gesture::TYPE_KEY_TAP);
-    controller.enableGesture(Gesture::TYPE_SCREEN_TAP);
     controller.enableGesture(Gesture::TYPE_SWIPE);
+    controller.enableGesture(Gesture::TYPE_SCREEN_TAP);
+    controller.enableGesture(Gesture::TYPE_KEY_TAP);
     controller.enableGesture(Gesture::TYPE_INVALID);
     
     controller.addListener(listener);
     
-    while(exCode){
-        if(!exCode){
+    while(!exCode){
+        if(exCode){
             controller.removeListener(listener);
             return 0;
         }
